995679f785cb8fdf47826070c98f4794a7c112aa
AlexB1988 <bochik_88@mail.ru>
2022-12-08 22:10:20 +0300

    Добавьте файлы проекта.

---
+++ ShopBaseApp/Domain/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShopBaseApp.Domain
+{
+    public class DataContext:DbContext
+    {
+        public DataContext()
+        {
+
+        }
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
